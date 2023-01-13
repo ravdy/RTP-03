2aced168ab54fbdf47db989b863d9f80e0526433
AR Shankar <valaxytech@gmail.com>
2023-01-13 12:35:20 +0530

    added git to slave-setup.sh file

--- slave-setup.sh
+++ slave-setup.sh
@@ -2,7 +2,6 @@
 pwd
 sudo yum update -y
 sudo amazon-linux-extras install java-openjdk11 -y 
-sudo yum install maven -y 
-sudo yum install docker -y 
+sudo yum install git maven docker -y 
 service docker start 
 chkconfig docker on 
